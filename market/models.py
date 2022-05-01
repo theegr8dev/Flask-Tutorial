64a3641be90e9fc6dee3b1bc1eae564e1e2019a2
Khalid Alabi <81325840+DeanSft-dev@users.noreply.github.com>
2022-05-01 21:40:56 +0100

    model relationship

--- market/models.py
+++ market/models.py
@@ -1,6 +1,16 @@
 from market import db
 
 
+class User(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    username = db.Column(db.String(length=30), unique=True, nullable=False)
+    email_address = db.Column(db.String(length=50),
+                              unique=True, nullable=False)
+    password_hash = db.Column(db.String(length=60), nullable=False)
+    budget = db.Column(db.Integer(), nullable=False,  default=1000)
+    items = db.relationship('Item', backref='owned_user', lazy=True)
+
+
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
@@ -8,6 +18,7 @@ class Item(db.Model):
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024),
                             nullable=False, unique=True)
+    owner = db.Column(db.Integer(), db.ForeignKey('user.id'))
 
     def __repr__(self):
         return f'Item {self.name}'
