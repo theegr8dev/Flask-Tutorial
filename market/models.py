584e9fa56bff8cddb8379054c6caa3ead3cb57a9
Khalid Alabi <81325840+DeanSft-dev@users.noreply.github.com>
2022-05-03 17:37:31 +0100

    Advance feature of item purchases added

--- market/models.py
+++ market/models.py
@@ -36,6 +36,9 @@ class User(db.Model, UserMixin):
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -48,3 +51,8 @@ class Item(db.Model):
 
     def __repr__(self):
         return f'Item {self.name}'
+
+    def buy(self, user):
+        self.owner = user.id
+        user.budget = user.budget - self.price
+        db.session.commit()
